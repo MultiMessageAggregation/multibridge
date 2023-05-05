d3f8bb446e91720926b816e0acab397e0c00ec16
Xiaozhou Li <xli@celer.network>
2023-05-04 17:19:28 -0700

    only allow single source chain

--- contracts/MultiMessageReceiver.sol
+++ contracts/MultiMessageReceiver.sol
@@ -7,12 +7,14 @@ import "./MessageStruct.sol";
 import "./interfaces/EIP5164/ExecutorAware.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
+// Receive messages from MultiMessageReceiver
 contract MultiMessageReceiver is IMultiMessageReceiver, ExecutorAware, Initializable {
+    // only allows a single source chain sender
+    uint256 public srcChainId;
+    address public multiMessageSender;
+
     // minimum accumulated power precentage for each message to be executed
     uint64 public quorumThreshold;
-
-    // srcChainId => multiMessageSender
-    mapping(uint256 => address) public multiMessageSenders;
 
     struct MsgInfo {
         bool executed;
@@ -50,30 +52,17 @@ contract MultiMessageReceiver is IMultiMessageReceiver, ExecutorAware, Initializ
     }
 
     /**
-     * @notice A modifier used for restricting that only messages sent from MultiMessageSender would be accepted.
-     */
-    modifier onlyFromMultiMessageSender() {
-        require(_msgSender() == multiMessageSenders[_fromChainId()], "this message is not from MultiMessageSender");
-        _;
-    }
-
-    /**
      * @notice A one-time function to initialize contract states.
      */
     function initialize(
-        uint256[] calldata _srcChainIds,
-        address[] calldata _multiMessageSenders,
+        uint256 _srcChainId,
+        address _multiMessageSender,
         address[] calldata _receiverAdapters,
         uint64 _quorumThreshold
     ) external initializer {
-        require(_multiMessageSenders.length > 0, "empty MultiMessageSender list");
-        require(_multiMessageSenders.length == _srcChainIds.length, "mismatch length");
         require(_receiverAdapters.length > 0, "empty receiver adapter list");
         require(_quorumThreshold <= _receiverAdapters.length, "invalid threshold");
-        for (uint256 i; i < _multiMessageSenders.length; ++i) {
-            require(_multiMessageSenders[i] != address(0), "MultiMessageSender is zero address");
-            _updateMultiMessageSender(_srcChainIds[i], _multiMessageSenders[i]);
-        }
+        _updateMultiMessageSender(_srcChainId, _multiMessageSender);
         for (uint256 i; i < _receiverAdapters.length; ++i) {
             require(_receiverAdapters[i] != address(0), "receiver adapter is zero address");
             _updateReceiverAdapter(_receiverAdapters[i], true);
@@ -87,19 +76,20 @@ contract MultiMessageReceiver is IMultiMessageReceiver, ExecutorAware, Initializ
      * this message will be executed immediately, which will invoke an external function call
      * according to the message content.
      */
-    function receiveMessage(
-        MessageStruct.Message calldata _message
-    ) external override onlyReceiverAdapter onlyFromMultiMessageSender {
+    function receiveMessage(MessageStruct.Message calldata _message) external override onlyReceiverAdapter {
+        uint256 _srcChainId = _fromChainId();
+        require(_srcChainId == srcChainId, "not from allowed source chain");
+        require(_msgSender() == multiMessageSender, "not from MultiMessageSender");
         require(_message.dstChainId == block.chainid, "dest chainId not match");
-        uint256 srcChainId = _fromChainId();
+
         // This msgId is totally different with each adapters' internal msgId(which is their internal nonce essentially)
         // Although each adapters' internal msgId is attached at the end of calldata, it's not useful to MultiMessageReceiver.
-        bytes32 msgId = MessageStruct.computeMsgId(_message, uint64(srcChainId));
+        bytes32 msgId = MessageStruct.computeMsgId(_message, uint64(_srcChainId));
         MsgInfo storage msgInfo = msgInfos[msgId];
         require(msgInfo.from[msg.sender] == false, "already received from this bridge adapter");
 
         msgInfo.from[msg.sender] = true;
-        emit SingleBridgeMsgReceived(msgId, srcChainId, _message.bridgeName, _message.nonce, msg.sender);
+        emit SingleBridgeMsgReceived(msgId, _srcChainId, _message.bridgeName, _message.nonce, msg.sender);
     }
 
     /**
@@ -156,14 +146,8 @@ contract MultiMessageReceiver is IMultiMessageReceiver, ExecutorAware, Initializ
      * This function can only be called by executeMessage() invoked within receiveMessage() of this contract,
      * which means the only party who can make these updates is the caller of the MultiMessageSender at the source chain.
      */
-    function updateMultiMessageSender(
-        uint256[] calldata _srcChainIds,
-        address[] calldata _multiMessageSenders
-    ) external onlySelf {
-        require(_srcChainIds.length == _multiMessageSenders.length, "mismatch length");
-        for (uint256 i; i < _multiMessageSenders.length; ++i) {
-            _updateMultiMessageSender(_srcChainIds[i], _multiMessageSenders[i]);
-        }
+    function updateMultiMessageSender(uint256 _srcChainId, address _multiMessageSender) external onlySelf {
+        _updateMultiMessageSender(_srcChainId, _multiMessageSender);
     }
 
     /**
@@ -219,7 +203,10 @@ contract MultiMessageReceiver is IMultiMessageReceiver, ExecutorAware, Initializ
     }
 
     function _updateMultiMessageSender(uint256 _srcChainId, address _multiMessageSender) private {
-        multiMessageSenders[_srcChainId] = _multiMessageSender;
+        require(_srcChainId != 0, "srcChainId is zero");
+        require(_multiMessageSender != address(0), "multiMessageSender is zero address");
+        srcChainId = _srcChainId;
+        multiMessageSender = _multiMessageSender;
         emit MultiMessageSenderUpdated(_srcChainId, _multiMessageSender);
     }
 }
